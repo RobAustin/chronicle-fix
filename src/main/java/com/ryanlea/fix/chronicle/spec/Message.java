970894b9af5fc676f44b267147d871a1c51d11db
lear <ryan.lea@gmail.com>
2013-12-17 22:56:06 +1100

    remove unused files.

---
+++ src/main/java/com/ryanlea/fix/chronicle/spec/Message.java
@@ -0,0 +1,11 @@
+package com.ryanlea.fix.chronicle.spec;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lear
+ * Date: 28/11/13
+ * Time: 8:23 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Message {
+}
